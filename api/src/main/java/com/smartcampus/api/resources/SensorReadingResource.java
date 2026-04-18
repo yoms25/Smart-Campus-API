f7a64b5f4ef568ff1459afc82ba7bd46c90ae600
Yomali Madanayake <yomalimadanayake@gmail.com>
2026-04-18 23:31:28 +0530

    implement sub-resource locator for sensor readings

---
+++ api/src/main/java/com/smartcampus/api/resources/SensorReadingResource.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.api.resources;
+
+/**
+ *
+ * @author user
+ */
+import com.smartcampus.api.data.DataStore;
+import com.smartcampus.api.exceptions.SensorUnavailableException;
+import com.smartcampus.api.models.Sensor;
+import com.smartcampus.api.models.SensorReading;
+import java.util.ArrayList;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SensorReadingResource {
+
+    private String sensorId;
+
+    // The constructor receives the context from the parent SensorResource
+    public SensorReadingResource(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    @GET
+    public Response getReadings() {
+        // Fetch the list directly using your upgraded DataStore structure
+        List<SensorReading> history = DataStore.getInstance().getSensorReadings().get(this.sensorId);
+        
+        // If there are no readings yet, return an empty list instead of null
+        if (history == null) {
+            history = new ArrayList<>();
+        }
+        
+        return Response.ok(history).build();
+    }
+
+    @POST
+    public Response addReading(SensorReading reading) {
+        Sensor parentSensor = DataStore.getInstance().getSensors().get(this.sensorId);
+        
+        if (parentSensor != null && "MAINTENANCE".equalsIgnoreCase(parentSensor.getStatus())) {
+            throw new SensorUnavailableException("Sensor is disconnected for maintenance.");
+        }
+
+        DataStore.getInstance().getSensorReadings()
+                .computeIfAbsent(this.sensorId, k -> new ArrayList<>())
+                .add(reading);
+
+        if (parentSensor != null) {
+            parentSensor.setCurrentValue(reading.getValue()); 
+        }
+
+        return Response.status(Response.Status.CREATED).entity(reading).build();
+    }
+}
